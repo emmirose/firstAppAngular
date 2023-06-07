47152984cd37ea527d3302d63061c784ae2bdc14
Emma Rosso <emmarosso.pro@gmail.com>
2023-06-07 15:16:44 +0200

    challenge form control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,7 @@ import { MyFormComponent } from './my-form/my-form.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from '../user';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,17 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent {
-  user = {
-    name : 'Doe',
-    firstName : 'John',
-    age : 25,
-    quote : '',
-    photo : 'https://randomuser.me/api/portraits/lego/2.jpg',
-  };
-  isShowDiv = false;
-  toggleDisplayDiv() {
-    this.isShowDiv = !this.isShowDiv;
-  }
-  songList: string[] = ["Alejandro", "Ne me quitte pas", "Le temps est bon", "Baby One more Time"];
+  user!: User;
+  username = new FormControl('');
+  email = new FormControl('');
+  password = new FormControl('');
+  street = new FormControl('');
+  zipcode = new FormControl('');
+  city = new FormControl('');
+  isDisplayed = false
 
+
+  createUser() {
+    this.user = new User (
+    this.username.value as string,
+    this.email.value as string,
+    this.password.value as string,
+    `${this.street.value}, ${this.zipcode.value}, ${this.city.value}`)
+    this.isDisplayed =true;
+  }
 }
